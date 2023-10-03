099af9e3e2b104432a27effeee91435821e3cc43
Nayuengin <65211260+Nayuengin@users.noreply.github.com>
2023-10-03 11:54:56 +0900

    Add: supported formats

--- script.sh
+++ script.sh
@@ -2,11 +2,19 @@
 
 cd /app/in
 
+SUPPORTED_FORMATS="jpg,jpeg,png,tiff,gif,bmp,yuv,jfif"
+
+IFS=',' read -ra FORMATS <<< "$SUPPORTED_FORMATS"
+
 echo "Converting images to webp..."
 
-for file in *.{jpg,png}; do cwebp -q 100 "$file" -o "/app/out/${file%.*}.webp"; done
-
-rm *.png
-rm *.jpg
+for format in "${FORMATS[@]}"; do
+    for file in *.$format; do
+        if [[ -f "$file" ]]; then
+            cwebp -q 100 "$file" -o "/app/out/${file%.*}.webp"
+            rm "$file"
+        fi
+    done
+done
 
 echo "Done!"
